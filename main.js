c74139b4acfe8f6bc8c2c31bfbf5965ac2a42edc
Malle <mallealaru@gmail.com>
2020-02-24 12:42:17 +0200

    text animation added

--- main.js
+++ main.js
@@ -11,3 +11,56 @@ $(document).on('click', 'ul li', function(){
 });
 
 
+var textWrapper = document.querySelector('.mov');
+textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+
+anime.timeline({loop: false})
+  .add({
+    targets: '.mov .letter',
+    opacity: [0,1],
+    easing: "easeInOutQuad",
+    duration: 900,
+    delay: (el, i) => 40 * (i+1)
+  }).add({
+    targets: '.mov',
+    opacity: 1,
+    duration: 700,
+    easing: "easeOutExpo",
+    delay: 100
+  });
+
+var textWrapper = document.querySelector('.lead');
+textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+
+anime.timeline({loop: false})
+  .add({
+    targets: '.lead .letter',
+    opacity: [0,1],
+    easing: "easeInOutQuad",
+    duration: 700,
+    delay: (el, i) => 55 * (i+1)
+  }).add({
+    targets: '.lead',
+    opacity: 1,
+    duration: 1000,
+    easing: "easeOutExpo",
+    delay: 700
+  });
+
+var textWrapper = document.querySelector('.lead2');
+textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+
+anime.timeline({loop: false})
+  .add({
+    targets: '.lead2 .letter',
+    opacity: [0,1],
+    easing: "easeInOutQuad",
+    duration: 500,
+    delay: (el, i) => 75 * (i+1)
+  }).add({
+    targets: '.lead2',
+    opacity: 1,
+    duration: 1000,
+    easing: "easeOutExpo",
+    delay: 3000
+  });
